1dfa7a08b51e3a4ff7e02c6f5750faec65724102
Juan Ignacio <jigutierrez004@gmail.com>
2026-04-17 22:29:20 -0300

    Arreglos de estilos

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
 
   async function handleSignOut() {
     await supabase.auth.signOut();
-    router.push("/");
+    router.push("/login");
     setSession(null);
   }
   useEffect(() => {
